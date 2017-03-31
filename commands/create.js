ec707536b23f6e69f75f61d3cd40d2e09d6f92ff
Tim Bailey <tim.bailey@noknow.nz>
2017-03-31 16:27:57 +1100

    Update default template to be 'Promised-based'

--- commands/create.js
+++ commands/create.js
@@ -16,30 +16,26 @@ class Create {
 
     var template = `
 var migration${this.dateString} = {
-  up : function (db, handler) {
-    var query = '';
-    var params = [];
-    db.execute(query, params, { prepare: true }, function (err) {
-      if (err) {
-        handler(err, false);
-      } else {
-        handler(false, true);
-      }
-    });
+  up: function (db, handler) {
+    return Promise.resolve()
+      .then(() => {
+        console.log(1)
+        return db.execute('')
+      })
+      .then(() => {
+        handler(null, 'Done')
+      })
+      .catch(err => {
+        console.error(err)
+        handler(err)
+      })
   },
-  down : function (db, handler) {
-    var query = '';
-    var params = [];
-    db.execute(query, params, { prepare: true }, function (err) {
-      if (err) {
-        handler(err, false);
-      } else {
-        handler(false, true);
-      }
-    });
+
+  down: function (db, handler) {
   }
-};
-module.exports = migration${this.dateString};`;
+}
+
+module.exports = migration${this.dateString}`;
 
     if (templateFile) {
       template = this.fs.readFileSync(templateFile);
